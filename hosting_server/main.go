31712d3e7bcef093a233a84c56a1a9cadac07e2b
Preston <101310062+colorfulparadox@users.noreply.github.com>
2024-10-04 11:51:43 -0700

    pain and suffering

--- hosting_server/main.go
+++ hosting_server/main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
-
 func main() {
-    fs := http.FileServer(http.Dir("../webapp/build"))
-    http.Handle("/", fs)
+	fs := http.FileServer(http.Dir("../webapp/build"))
+	//http.Handle("/", fs)
 
-    fmt.Println("Starting server!")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Handle all of the requests
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := filepath.Join("../webapp/build", r.URL.Path)
+
+		// Check if the requested file exists and if they do not serve index.html
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			http.ServeFile(w, r, "../webapp/build/index.html")
+			return
+		}
+		fs.ServeHTTP(w, r)
+	})
+
+	fmt.Println("Starting server!")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
