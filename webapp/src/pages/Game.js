3e22f62df96454bef31f559793e6897334dfcf71
colorfulparadox <101310062+colorfulparadox@users.noreply.github.com>
2024-10-06 11:56:09 -0700

    added unity-demo-game

--- webapp/src/pages/Game.js
+++ webapp/src/pages/Game.js
@@ -1,10 +1,23 @@
 import NavBar from '../components/NavBar'; 
 
+//import logo from '../assets/logo.png' 
+
+import { Unity, useUnityContext } from "react-unity-webgl";
+
 export default function Game() {
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "UnityBuild/build.loader.js",
+        dataUrl: "UnityBuild/build.data",
+        frameworkUrl: "UnityBuild/build.framework.js",
+        codeUrl: "UnityBuild/build.wasm",
+      });
+
     return (
         <>
             <NavBar />
-            <h1>This is the game view!</h1>
+            <div className="LoginMain">
+                <Unity unityProvider={unityProvider} style={{ width: 960, height: 600 }}/>
+            </div>
         </>
     )
 }
