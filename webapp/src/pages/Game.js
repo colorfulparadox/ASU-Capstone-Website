037adf5a828b7f4839bbc02a6a94720e6db109c4
Preston <101310062+colorfulparadox@users.noreply.github.com>
2025-04-18 10:22:03 -0700

    added user guide

--- webapp/src/pages/Game.js
+++ webapp/src/pages/Game.js
@@ -19,13 +19,12 @@ export default function Game() {
             <div className="LoginMain">
                 <div>
                     <h1>Game Guide</h1>
-                    <p>asdasdasdasdasd</p>
-                    <h3>How to Install</h3>
-                    <p>asdasdasdasdasd</p>
-                    <h3>How to Login</h3>
-                    <p>asdasdasdasdasd</p>
-                    <h3>Customer Interactions</h3>
-                    <p>asdasdasdasdasd</p>
+                    <h3>Logging In</h3>
+                    <p>User will have to navigate to the Login Button. Once on the Login Screen the user should enter their username and password. Once the user has entered their information the user will click 'Login'. </p>
+                    <h3>Playing the Game</h3>
+                    <p>As the user plays the game customers will walk in. Customers will find a seat to sit at and then the user will click on the customer to interact with them.</p>
+                    <h4>Customer Interactions</h4>
+                    <p>Users will interact with customers by conducting a short quiz and then Once thecommunicating to them through a textbox prompt. Once the user is done interacting with the customer the user will exit the interaction and the customer will leave.</p>
                 </div>
             </div>
         </>
