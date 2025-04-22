47a4c8f3022d83a6001604b3748e96a2eea216dc
Skyler Riske <sriske2@users.noreply.github.com>
2025-04-22 09:45:47 -0700

    update user guide formatting

--- webapp/src/pages/Game.js
+++ webapp/src/pages/Game.js
@@ -1,4 +1,5 @@
 import NavBar from '../components/NavBar'; 
+import { Container, Row, Col, Image, Form, ListGroup, Button } from 'react-bootstrap';
 
 //import logo from '../assets/logo.png' 
 
@@ -17,15 +18,21 @@ export default function Game() {
         <>
             <NavBar />
             <div className="LoginMain">
-                <div>
-                    <h1>Game Guide</h1>
-                    <h3>Logging In</h3>
-                    <p>User will have to navigate to the Login Button. Once on the Login Screen the user should enter their username and password. Once the user has entered their information the user will click 'Login'. </p>
-                    <h3>Playing the Game</h3>
-                    <p>As the user plays the game customers will walk in. Customers will find a seat to sit at and then the user will click on the customer to interact with them.</p>
-                    <h4>Customer Interactions</h4>
-                    <p>Users will interact with customers by conducting a short quiz and then Once thecommunicating to them through a textbox prompt. Once the user is done interacting with the customer the user will exit the interaction and the customer will leave.</p>
-                </div>
+            <Container>
+                <Row>
+                        <h1>Project Persona User Guide</h1>
+                    <Col style={{ padding: '20px' }}>
+                    <div>
+                        <h3>Logging In</h3>
+                        <p>User will have to navigate to the Login Button. Once on the Login Screen the user should enter their username and password. Once the user has entered their information the user will click 'Login'. </p>
+                        <h3>Playing the Game</h3>
+                        <p>As the user plays the game customers will walk in. Customers will find a seat to sit at and then the user will click on the customer to interact with them.</p>
+                        <h4>Customer Interactions</h4>
+                        <p>Users will interact with customers by conducting a short quiz and then interacting with them through the UI. Once the user is done interacting with the customer the user will exit the interaction and the customer will leave.</p>
+                    </div>
+                </Col>
+                </Row>
+            </Container>
             </div>
         </>
     )
